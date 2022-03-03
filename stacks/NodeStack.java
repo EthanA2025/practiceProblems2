2cd341e5256761540ebd2c3b1c07792e58ff6c52
etan Memeignton <eja9979@g.rit.edu>
2022-03-02 22:38:53 -0500

    stubbed out nodestack

---
+++ stacks/NodeStack.java
@@ -0,0 +1,29 @@
+package stacks;
+
+public class NodeStack<E> implements Stack<E> {
+
+    @Override
+    public void push(E value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public E top() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E pop() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
